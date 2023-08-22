977e94d1b07037b67b868571a3ee3284cfeb2138
Boieruu <olteanuliviu702@yahoo.com>
2023-08-22 18:55:53 +0300

    eroare weather

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  WeatherData:any;
+  
+  constructor() { }
+
+  ngOnInit() {
+    this.WeatherData = {
+      main : {},
+      isDay: true
+    };
+    this.getWeatherData();
+    console.log(this.WeatherData);
+  }
+
+  getWeatherData(){
+    fetch('api.openweathermap.org/data/2.5/weather?q=targu-jiu&appid=63fdce08f5b8339ef0c50c1e9e914dc5')
+    .then(response=>response.json())
+    .then(data=>{this.setWeatherData(data);})
+
+  }
+
+  setWeatherData(data){
+    this.WeatherData = data;
+    let sunsetTime = new Date(this.WeatherData.sys.sunset * 1000);
+    this.WeatherData.sunset_time = sunsetTime.toLocaleTimeString();
+    let currentDate = new Date();
+    this.WeatherData.isDay = (currentDate.getTime() < sunsetTime.getTime());
+    this.WeatherData.temp_celcius = (this.WeatherData.main.temp - 273.15).toFixed(0);
+    this.WeatherData.temp_min = (this.WeatherData.main.temp_min - 273.15).toFixed(0);
+    this.WeatherData.temp_max = (this.WeatherData.main.temp_max - 273.15).toFixed(0);
+    this.WeatherData.temp_feels_like = (this.WeatherData.main.feels_like - 273.15).toFixed(0);
+  }
 
 }
