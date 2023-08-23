578fd15f85e69f5edfe5e01ea786380088bf1d5e
Alex6227 <ilincaalex6227@gmail.com>
2023-08-23 21:26:58 +0300

    about/readmore

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { AboutComponent } from './about/about.component';
 import { WorldMapComponent } from './world-map/world-map.component';
 import { UywComponent } from './uyw/uyw.component';
 import { ContactComponent } from './contact/contact.component';
+import { ReadmoreComponent } from './about/readmore/readmore.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -9,6 +9,11 @@ import { AboutComponent } from './about/about.component';
 import { WorldMapComponent } from './world-map/world-map.component';
 import { UywComponent } from './uyw/uyw.component';
 import { ContactComponent } from './contact/contact.component';
+import { ReadmoreComponent } from './about/readmore/readmore.component';
+
+const routes: Routes = [
+  { path: 'readmore', component: ReadmoreComponent },
+];
 
 @NgModule({
   declarations: [
@@ -18,12 +23,15 @@ import { ContactComponent } from './contact/contact.component';
     AboutComponent,
     WorldMapComponent,
     UywComponent,
-    ContactComponent
+    ContactComponent,
+    ReadmoreComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes), 
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
