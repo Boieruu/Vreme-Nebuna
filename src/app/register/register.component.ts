bb9ea26fd136daf02a9efba80da4168de1b4183d
Boieruu <olteanuliviu702@yahoo.com>
2023-10-09 11:56:59 -0700

    merge

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from '../services/user.service';
+import { UserRegister } from '../interfaces/UserRegister';
 
 @Component({
   selector: 'app-register',
@@ -19,4 +20,33 @@ export class RegisterComponent {
     private router: Router,
     private userService: UserService
   ) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+
+    this.returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'];
+  }
+
+  get fc() {
+    return this.registerForm.controls;
+  }
+
+  submit() {
+    this.isSubmitted = true;
+    if (this.registerForm.invalid) return;
+    const fv = this.registerForm.value;
+    const user: UserRegister = {
+      name: fv.name,
+      email: fv.email,
+      password: fv.password,
+    };
+
+    this.userService.register(user).subscribe((_) => {
+      this.router.navigateByUrl(this.returnUrl);
+    });
+  }
 }
